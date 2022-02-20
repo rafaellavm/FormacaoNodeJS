bd28b21d9108f99a0aaed38e20492c3dc751ea37
Rafaela <rafaela.vm@gmail.com>
2022-02-20 11:57:09 -0400

    11 Rotas

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,22 @@
 const express = require("express"); //importando o express
 const app = express(); //iniciando o express
 
+//criando uma rota
+app.get("/", function(requisicao, resposta) {
+
+    resposta.send("Bem-vindo ao guia do programador!");
+});
+
+app.get("/blog", function(requisicao, resposta) {
+
+    resposta.send("Bem-vindo ao meu blog!");
+});
+
+app.get("/canal/youtube", function(requisicao, resposta) {
+
+    resposta.send("Bem-vindo ao meu canal!");
+});
+
 app.listen(400, function(erro) {
 
     if (erro) {
