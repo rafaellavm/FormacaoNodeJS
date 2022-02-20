2f899242d6d709e9cc60e94aab9e59eb5a8c3430
Rafaela <rafaela.vm@gmail.com>
2022-02-20 15:47:28 -0400

    13 Parâmetros

--- Express/index.js
+++ Express/index.js
@@ -4,7 +4,7 @@ const app = express(); //iniciando o express
 //criando uma rota
 app.get("/", function(requisicao, resposta) {
 
-    resposta.send("Bem-vindo ao guia do programador!");
+    resposta.send("<h2>Bem-vindo ao guia do programador!!!!</h2>");
 });
 
 app.get("/blog", function(requisicao, resposta) {
@@ -16,6 +16,34 @@ app.get("/canal/youtube", function(requisicao, resposta) {
 
     resposta.send("Bem-vindo ao meu canal!");
 });
+
+// colocar parametros
+app.get("/ola/:nome/:empresa", function(requisicao, resposta) {
+
+    //requisicao => dados enviados pelo usuario
+    // resposta => Resposta que vai ser enviada pelo usuário
+    var nome = requisicao.params.nome;
+    var empresa = requisicao.params.empresa;
+
+    resposta.send("<h1>Oi, " + nome + ", da empresa: " + empresa + "</h1>");
+});
+
+//parâmetros não obrigatórios = usando o '?'
+app.get("/cadastro/:artigo?", function(requisicao, resposta) {
+
+    var artigo = requisicao.params.artigo;
+
+    if (artigo) {
+        resposta.send("<h1>Oi, essa é a área de cadastro do site! O nome do artigo é: <strong>'" + artigo + "'</strong></h1>");
+
+    } else {
+        resposta.send("<h1>Oi, essa é a área de cadastro do site! </h1>");
+
+    }
+
+    //resposta.send("<h1>Oi, essa é a área de cadastro do site! </h1>");
+});
+
 
 app.listen(400, function(erro) {
 
