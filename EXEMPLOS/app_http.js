73c62e18b7b377d3e46536688b0feba1a69f15f5
Rafaela <rafaela.vm@gmail.com>
2022-02-20 11:01:55 -0400

    correção gitignore

--- EXEMPLOS/app_http.js
+++ EXEMPLOS/app_http.js
@@ -4,4 +4,5 @@ http.createServer(function(requisicao, resposta) {
     resposta.end("<h1>Bem-vindo ao meu site!</h1><br><h4>Guia do programador</h4>");
 }).listen(8181);
 
+
 console.log("Meu servidor está logando");
